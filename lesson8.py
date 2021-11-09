caa210abdddd82cc3c3ac47ce43312449a8e4175
YuriiCherevko <cherevko2503@gmail.com>
2021-11-09 23:13:54 +0200

    lesson8~2

--- lesson8.py
+++ lesson8.py
@@ -9,9 +9,16 @@ peters_height = int(input())
 
 for i, ch in enumerate(class_list):
     if i == len(class_list) - 1:
+        class_list.append(peters_height)
         print("Порядковый номер Пети:")
         print(i + 2)
+        print("Список класса с Петей:")
+        print(class_list)
+        break
     elif peters_height > ch:
+        class_list.insert(i, peters_height)
         print("Порядковій номер Пети:")
         print(i + 1)
+        print("Список класса с Петей:")
+        print(class_list)
         break
