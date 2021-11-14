b183fdcbecdec6dc7a0d7d6924f6303551c75956
YuriiCherevko <cherevko2503@gmail.com>
2021-11-14 20:54:26 +0200

    lesson10~2

--- classLessons.py
+++ classLessons.py
@@ -1,19 +1,11 @@
-data = {
-        "model": "services.service",
-        "pk": 2236,
-        "fields": {
-            "category": 17,
-            "name": "Спецификация отделочных материалов",
-            "unit": 3,
-            "min_price": 1,
-            "max_price": 1000
-        }
-    }
-for k, v in data.items():
-    if isinstance(v, dict):
-        for k1, v1 in v.items():
-            if k1 == 'name':
-                print(v1)
+def catalog_vehicle(a, b):
+    print(a)
+    return a * b
 
 
-
+if __name__ == "__main__":
+    res = catalog_vehicle(3, 2)
+    print(res)
+#
+# res = catalog_vehicle(3, 2)
+# print(res)
--- lesson10.py
+++ lesson10.py
@@ -1,8 +1,6 @@
 # Для проверки
-# set_1 = {1, 2, 3, 4, 5}
-set_1 = {1, 2, 3, 4}
+set_1 = {1, 2, 3, 4, 5}
+# set_1 = {1, 2, 3, 4}
 set_2 = {2, 3, 4, 5}
-superset = {*set_1, *set_2}
 
-is_superset = True if set_1 == superset else False
-print(is_superset)
+print(set_1 >= set_2)
